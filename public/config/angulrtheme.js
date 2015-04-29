3d93bd27baadcded859e69ade8777a19e09fd74d
Carlos <carlosthe19916@gmail.com>
2015-04-29 13:03:06 -0500

    add restfull url

--- public/config/angulrtheme.js
+++ public/config/angulrtheme.js
@@ -1,6 +1,23 @@
 'use strict';
 
 /* jshint -W098 */
+
+angular.module('mean.angulrtheme').config(function(sgIso3166Provider) {
+  sgIso3166Provider.restUrl = 'http://localhost:8080/iso3166/rest';
+});
+
+angular.module('mean.angulrtheme').config(function(sgIso4217Provider) {
+  sgIso4217Provider.restUrl = 'http://localhost:8080/iso4217/rest';
+});
+
+angular.module('mean.angulrtheme').config(function(sgProductoProvider) {
+  sgProductoProvider.restUrl = 'http://localhost:8080/producto/rest';
+});
+
+angular.module('mean.angulrtheme').config(function(sgPersonaProvider) {
+  sgPersonaProvider.restUrl = 'http://localhost:8080/persona/rest';
+});
+
 angular.module('mean.angulrtheme').run(function(editableOptions, editableThemes) {
   editableThemes.bs3.inputClass = 'input-sm form-control';
   editableThemes.bs3.buttonsClass = 'btn-sm';
